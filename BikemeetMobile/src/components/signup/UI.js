ecf56e1f6b20d0aae485b75a66c265dd9188a5f0
Leonardo J. Perez <find.leonardo@gmail.com>
2018-07-05 17:34:07 -0700

    Updated Signup ui.

--- BikemeetMobile/src/components/signup/UI.js
+++ BikemeetMobile/src/components/signup/UI.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TextInput, View, Picker} from 'react-native';
 
 import Avatar from '../avatar';
 
 import * as actions from './actions';
 
 class Signup extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: 'Useless Placeholder',
+            state: '',
+            country: ''
+        };
+    }
+
     // Set up Linking
     componentDidMount() {
         console.log("Checking Location services");
@@ -21,58 +30,89 @@ class Signup extends Component {
         return (
             <View style={styles.container}>
                 <View style={styles.content}>
-                    <Avatar onClick={changeAvatar} image={user.avatar} name={user.name}/>
-                </View>
-                <View style={styles.content}>
                     <Text style={styles.header}>
-                        Fields need to be added.
+                        Welcome {user.name || "Stranger"}!
                     </Text>
+                    <View style={styles.avatar}>
+                        <Avatar onClick={changeAvatar} image={user.avatar} name={user.name}/>
+                    </View>
+                    <View style={styles.form}>
+                        <TextInput
+                            style={styles.textbox}
+                            placeholder={"Email"}
+                            onChangeText={(text) => this.setState({text})}
+                            value={user.email}/>
+                        <TextInput
+                            style={styles.textbox}
+                            placeholder={"Full Name"}
+                            onChangeText={(text) => this.setState({text})}
+                            value={user.name}/>
+                        <TextInput
+                            style={styles.textbox}
+                            maxLength={40}
+                            placeholder={"Birthdate"}
+                            onChangeText={(text) => this.setState({text})}/>
+                        <TextInput
+                            style={styles.textbox}
+                            maxLength={40}
+                            placeholder={"Gender"}
+                            onChangeText={(text) => this.setState({text})}/>
+                        <Picker
+                            selectedValue={this.state.state}
+                            mode={'dialog'}
+                            onValueChange={(itemValue, itemIndex) => this.setState({state: itemValue})}>
+                            <Picker.Item label="Select a State" value=""/>
+                            <Picker.Item label="Georgia" value="georgia"/>
+                            <Picker.Item label="Washington" value="washington"/>
+                        </Picker>
+                        <Picker
+                            selectedValue={this.state.country}
+                            mode={'dialog'}
+                            onValueChange={(itemValue, itemIndex) => this.setState({country: itemValue})}>
+                            <Picker.Item label="Select a Country" value=""/>
+                            <Picker.Item label="US" value="us"/>
+                            <Picker.Item label="Canada" value="canada"/>
+                        </Picker>
+                    </View>
                 </View>
             </View>
         );
     }
 }
 
-const iconStyles = {
-    borderRadius: 10,
-    iconStyle: {
-        paddingVertical: 5
-    }
-};
-
 const styles = StyleSheet.create({
     container: {
+        backgroundColor: '#5e5c5c',
         flex: 1,
-        backgroundColor: '#FFF'
+        padding: 10
     },
     content: {
+        alignItems: 'center',
+        backgroundColor: '#FFF',
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    avatar: {
-        margin: 20
-    },
-    avatarImage: {
-        borderRadius: 50,
-        height: 100,
-        width: 100
+        justifyContent: 'flex-start',
+        paddingTop: 30
     },
     header: {
         fontSize: 20,
-        textAlign: 'center',
-        margin: 10
+        marginBottom: 30,
+        textAlign: 'center'
     },
-    text: {
-        textAlign: 'center',
-        color: '#333',
-        marginBottom: 5
+    avatar: {
+        paddingBottom: 30
     },
-    buttons: {
-        justifyContent: 'space-between',
-        flexDirection: 'row',
-        margin: 20,
-        marginBottom: 30
+    form: {
+        width: 350
+    },
+    textbox: {
+        alignSelf: 'stretch',
+        borderColor: 'gray',
+        borderWidth: 1,
+        height: 35,
+        marginBottom: 5,
+        marginLeft: 10,
+        marginRight: 10,
+        padding: 5
     }
 });
 
