3771df111ba98c251e0f862e05f4ff953d626a38
Leonardo J. Perez <find.leonardo@gmail.com>
2018-07-19 15:00:56 -0700

    Simplified db sync.

--- BikemeetBackend/src/db/sync.js
+++ BikemeetBackend/src/db/sync.js
@@ -1,27 +1,11 @@
-import Sequelize from 'sequelize';
+import db from '.';
 
-const sequelize = new Sequelize(connString, {
-    dialect: 'mysql',
-    operatorsAliases: false,
-
-    pool: {
-        max: 10,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-});
-
-database.sequelize
-    .query('CREATE DATABASE IF NOT EXISTS `bikemeet`;')
-    .then(data => {
-        database.sequelize.sync()
-            .then(() => {
-                console.log('`bikemeet` schema successfully created/updated.');
-                return process.exit(0);
-            });
+db().sequelize.sync()
+    .then(() => {
+        console.log('`bikemeet` schema tables successfully created/updated.');
+        return process.exit(0);
     })
     .catch(err => {
-        console.error('Unable to create `bikemeet` schema:', err);
+        console.error('Unable to create `bikemeet` schema tables:', err);
         return process.abort();
     });
